87c51992baf366bd47082a975bbcee7a5f76edb1
George Lee <keoki.lee@gmail.com>
2015-08-15 13:53:15 -1000

    Finished step 1.

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,21 @@
 // Add your index.js code in this file
+'use strict';
+
+var soundButtons = document.querySelectorAll('.button-sound');
+var forEach = Array.prototype.forEach;
+
+forEach.call(soundButtons, function (button) {
+  var name = button.attributes['data-sound'].value;
+  prepareButton(button, name);
+});
+
+function prepareButton(buttonEl, soundName) {
+  var url = 'url("img/icons/' + soundName + '.png")';
+  var audio = new Audio(__dirname + '/wav/' + soundName + '.wav');
+
+  buttonEl.querySelector('span').style.backgroundImage  = url;
+  buttonEl.addEventListener('click', function() {
+    audio.currentTime = 0;
+    audio.play();
+  });
+}
